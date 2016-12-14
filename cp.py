95c718dd7e765cffa4d0d379b5805139ec5ead56
George <liusw@carit.com.cn>
2016-12-14 16:27:17 +0800

    add .cpignore

--- cp.py
+++ cp.py
@@ -1,8 +1,28 @@
 import os
 import sys
 import os.path
+
+ignores=[]
+if os.path.exists(".cpignore") :
+    ignorefile = open(".cpignore","r")
+    global ignores 
+    ignores = ignorefile.readlines();
+    ignorefile.close()
+
 for root, dirs, files in os.walk(sys.argv[1]):
 	for name in files:
+                global ignores
+                isignore = False
+                for path in ignores:
+                    path = path.strip('\n')
+                    if cmp(root[:len(path)],path)== 0  :
+                        isignore = True
+                        break;
+                if isignore :
+                    continue
+                if not os.path.exists(root+'/'+name):
+                    continue
+
 		if not os.path.exists(sys.argv[2]+root[len(sys.argv[1]):len(root)]) :
 			#print "create "+ sys.argv[2]+root[len(sys.argv[1]):len(root)]
 			os.makedirs(sys.argv[2]+root[len(sys.argv[1]):len(root)])
